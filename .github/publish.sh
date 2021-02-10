73fb7c0bc9a137635e362bc4e302f17d789aca53
Andrew Svoboda <asvoboda@palantir.com>
2021-02-10 18:16:11 +0000

    Actually publish the tgz

--- .github/publish.sh
+++ .github/publish.sh
@@ -8,4 +8,4 @@ echo "testing v ${v}" > foo.txt
 tar -zcvf foo.tgz foo.txt
 
 curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.txt"
-curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.tgz"
+curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.tgz "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.tgz"
