c95109c176e5dfff781ff7e9b3df566dd137e7ec
Andrew Svoboda <asvoboda@palantir.com>
2021-02-10 17:07:35 +0000

    fix maven2 layout

--- .github/publish.sh
+++ .github/publish.sh
@@ -5,4 +5,4 @@
 echo "test" > foo.txt
 v=$(git describe --tags --always --first-parent)
 
-curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/${v}/funicular-$v.txt"
+curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.txt"
