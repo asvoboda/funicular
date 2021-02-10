30da997eb9fa3530bbd9f143314a090a64036a8a
Andrew Svoboda <asvoboda@palantir.com>
2021-02-10 17:09:29 +0000

    Try to upload a tgz

--- .github/publish.sh
+++ .github/publish.sh
@@ -2,7 +2,10 @@
 
 ./gradlew publish
 
-echo "test" > foo.txt
 v=$(git describe --tags --always --first-parent)
+echo "testing v ${v}" > foo.txt
+
+tar -zcvf foo.tgz foo.txt
 
 curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.txt"
+curl -XPUT -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -T foo.txt "https://maven.pkg.github.com/asvoboda/funicular/org/asvoboda/funicular/${v}/funicular-$v.tgz"
